5bfafc728faaf8aceeb039c487fc2a0a96948e10
Anurag Singh <anusingh29@WKMZT9AEC488.global.publicisgroupe.net>
2023-08-21 15:10:41 +0530

    integrating state management

--- app-to-use/src/components/App.js
+++ app-to-use/src/components/App.js
@@ -1,27 +1,19 @@
-import * as React from 'react';
-import { useEffect } from 'react';
-import './styles.css';
+import * as React from "react";
+import "./styles.css";
 //import mitt from 'mitt'
-import Counter from './Counter';
-
-
-//import useCounterStore from 'MICRO/store';
-
+import Counter from "./Counter";
+import useCounterStore from "MICRO/store";
 
 function App({ onChange }) {
-    //console.log("Store content:", useCounterStore())
-    const [count, setCount] = React.useState(0);
-    const increment = () => setCount(count+1)
-    const decrement = () => setCount(count-1)
-    //const {count, increment, decrement} = useCounterStore();
-    return ( 
-        <div className='MicroApp'>
-            <label>Counter Value: {count}</label>
-             <Counter increment={increment} decrement={decrement}/>
-            <h1>Micro App</h1>
-            <input onChange={onChange} type="text" placeholder="Enter your name"/>
-        </div>
-     );
+  const { count, increment, decrement } = useCounterStore();
+  return (
+    <div className="MicroApp">
+      <label>Counter Value: {count}</label>
+      <Counter increment={increment} decrement={decrement} />
+      <h1>Micro App</h1>
+      <input onChange={onChange} type="text" placeholder="Enter your name" />
+    </div>
+  );
 }
 
 export default App;
