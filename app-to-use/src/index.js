dad33e48837cec9318e85216d716a761903c13ae
Anurag Singh <anusingh29@WKMZT9AEC488.global.publicisgroupe.net>
2023-08-21 15:36:42 +0530

    integrating state management

--- app-to-use/src/index.js
+++ app-to-use/src/index.js
@@ -1,12 +1,1 @@
-import React  from 'react';
-import { createRoot } from 'react-dom/client';
-import MainApp from './components/Main';
-
-const rootElement = document.getElementById("container");
-const root = createRoot(rootElement);
-
-root.render(
-    <React.StrictMode>
-        <MainApp/>
-    </React.StrictMode>
-);
+import("./bootstrap");
